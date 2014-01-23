72f1be2cedb80f98f4ea0281b13ad300166a30bf
Kandice Wong <kyw009@ucsd.edu>
2014-01-23 10:52:04 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,45 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+	$("a.thumbnail").click(projectClick);
+
+	$("#submitBtn").click(formSubmit);	
+}
+
+function projectClick(e) { 
+	//print string to browser's console
+	console.log("Project clicked");
+    // prevent the page from reloading 
+    e.preventDefault();
+    // In an event handler, $(this) refers to 
+    // the object that triggered the event 
+    $(this).css("background-color", "#7fff00");
+
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else if(description.css("display") == "none"){ 
+       description.fadeIn();
+    } else {
+    	description.fadeOut();
+    }
+}
+
+function formSubmit(e) {
+	console.log("Submit button clicked");
+	var selectedProject = $('#project').val();
+	$(selectedProject).animate({
+		width: $('#width').val()
+	});
+
+	var newText = $('#description').val();
+	$(selectedProject + " .project-description").text(newText);
 }
